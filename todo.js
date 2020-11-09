07d6b7845e8acc34d34e619d02db381eaa0e5e63
Frank Atukunda <fatukunda@gmail.com>
2020-11-09 10:07:46 +0300

    Mark a todo as completed.

--- todo.js
+++ todo.js
@@ -2,18 +2,22 @@ const todos = [
   {
     id: 1,
     item: "Wake Up",
+    completed: true,
   },
   {
     id: 2,
     item: "Eat breakfast",
+    completed: false,
   },
   {
     id: 3,
     item: "Go to work",
+    completed: false,
   },
   {
     id: 4,
     item: "Go Swimming",
+    completed: false,
   },
 ];
 
@@ -25,6 +29,10 @@ const createTodoElement = (todo) => {
 
   const todoTextElement = document.createElement("p");
   todoTextElement.innerHTML = todo.item;
+  if (todo.completed === true) {
+    todoTextElement.style.textDecoration = "line-through";
+    todoTextElement.style.color = "blue";
+  }
 
   todoItem.appendChild(todoTextElement);
 
@@ -34,12 +42,20 @@ const createTodoElement = (todo) => {
   const completedBtn = document.createElement("button");
   completedBtn.classList.add("completed-btn");
   completedBtn.innerHTML = "✓";
+  completedBtn.style.cursor = "pointer";
+
+  completedBtn.addEventListener("click", () => {
+    todoTextElement.style.textDecoration = "line-through";
+    todoTextElement.style.color = "blue";
+    todo.completed = true;
+  });
 
   todoItemButtons.appendChild(completedBtn);
 
   const deleteBtn = document.createElement("button");
   deleteBtn.classList.add("delete-btn");
   deleteBtn.innerHTML = "x";
+  deleteBtn.style.cursor = "pointer";
 
   todoItemButtons.appendChild(deleteBtn);
 
@@ -58,11 +74,16 @@ const saveButton = document.getElementById("save-btn");
 
 saveButton.addEventListener("click", () => {
   let newTodoInputValue = newTodoInput.value;
-  const todoItem = {
-    id: todos.length + 1,
-    item: newTodoInputValue,
-  };
-  todos.push(todoItem);
-  createTodoElement(todoItem);
-  newTodoInput.value = "";
+  if (newTodoInputValue === "") {
+    alert("A todo is required.");
+  } else {
+    const todoItem = {
+      id: todos.length + 1,
+      item: newTodoInputValue,
+      completed: false,
+    };
+    todos.push(todoItem);
+    createTodoElement(todoItem);
+    newTodoInput.value = "";
+  }
 });
